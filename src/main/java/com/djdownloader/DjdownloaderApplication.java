e149e2da68b7e960ea76f38182865a88e91d79ca
Michelle Jauge <asesinevil@gmail.com>
2025-02-01 01:13:30 -0300

    First Commit

---
+++ src/main/java/com/djdownloader/DjdownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.djdownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjdownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjdownloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djdownloader/DjdownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djdownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjdownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
